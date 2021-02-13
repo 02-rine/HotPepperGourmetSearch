5c7a9faf3b11bc1d9c6ef6b610b8f33a55307d6c
r203323 <kobayashi.reni.ot@tut.jp>
2021-02-13 14:24:03 +0900

    refactor: 変数にコメントを追加

--- app/src/main/java/com/example/hotpeppergourmetsearch/SearchData.kt
+++ app/src/main/java/com/example/hotpeppergourmetsearch/SearchData.kt
@@ -4,10 +4,10 @@ import java.io.Serializable
 
 // ホットペッパーAPIの検索クエリを保存するData class
 data class SearchData(
-        var feeCode1: String,
-        var feeCode2: String,
-        var range: Int,
-        var key: String,
-        var latitude: Double,
-        var longitude: Double
+        var feeCode1: String, // 予算コード１
+        var feeCode2: String, // 予算コード２
+        var range: Int, // 検索範囲
+        var key: String, // APIキー
+        var latitude: Double, // 緯度
+        var longitude: Double // 経度
 ): Serializable
